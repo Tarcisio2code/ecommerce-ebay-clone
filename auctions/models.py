8da63eb66aaacf9576ed80f68861a5f530cf0b80
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2022-12-05 12:01:28 +0100

    Add Category, Listing Models and Admin interface

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=50)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    imageUrl = models.CharField(max_length=1000)
+    price = models.FloatField()
+    isActive = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
